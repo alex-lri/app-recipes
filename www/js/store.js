0895dc06866f3c682bff8c3ccb61db2ef3815937
Alexandre Lri <alexandre.lairie@gmail.com>
2021-11-24 14:50:26 +0100

    [ADD] - Finish Add Wishlist

--- www/js/store.js
+++ www/js/store.js
@@ -1,23 +1,24 @@
-
-
 var createStore = Framework7.createStore;
 const store = createStore({
-  state: {
-    categories: [],
-    favoriteRecipes: [],
-  },
-  getters: {
-    categories({ state }) {
-      return state.categories;
+    state: {
+        categories: [],
+        favoriteRecipes: [],
     },
-    favoriteRecipes({ state }) {
-      return state.favoriteRecipes;
+    getters: {
+        categories({ state }) {
+            return state.categories;
+        },
+        favoriteRecipes({ state }) {
+            return state.favoriteRecipes;
+        },
     },
-  },
-  actions: {
-    addCategory({ state }, category) {
-      state.categories = [...state.categories, category];
-    },
-  },
-})
+    actions: {
+        addCategory({ state }, category) {
+            state.categories = [...state.categories, category];
+        },
 
+        addFavoriteRecipe({ state }, recipe) {
+            state.favoriteRecipes = [...state.favoriteRecipes, recipe];
+        },
+    },
+});
