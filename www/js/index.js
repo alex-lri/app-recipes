2f8394ad1217b411790a54883988acc95a0c6650
Xamimus <pro.maximemartin@gmail.com>
2021-11-25 10:02:20 +0100

    init searchbar

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,22 @@
 function searchByName(name) {
+    let ul = document.getElementById('list-results')
     const searchUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="+name;
+    while (ul.hasChildNodes()) {  
+        ul.removeChild(ul.firstChild);
+    }
 
     Framework7.request.get(searchUrl).then((res) => {
         let recipes = JSON.parse(res.data)
         console.log(recipes.meals)
         for(let r of recipes.meals){
             console.log("One recipe : ",r)
+            let liHtml = '<li class="item-content"><div class="item-inner"><div class="item-title">'+ r.strMeal +'</div></div></li>';
+            ul.insertAdjacentHTML("afterBegin",liHtml);
         }
     });
+}
+
+function getTitle(title){
+    break
 }
 
