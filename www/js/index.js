c238104462b55814a258ba33e66e7b9f549afb2b
Xamimus <pro.maximemartin@gmail.com>
2021-11-25 10:50:15 +0100

    edit searchbar

--- www/js/index.js
+++ www/js/index.js
@@ -7,16 +7,11 @@ function searchByName(name) {
 
     Framework7.request.get(searchUrl).then((res) => {
         let recipes = JSON.parse(res.data)
-        console.log(recipes.meals)
         for(let r of recipes.meals){
-            console.log("One recipe : ",r)
-            let liHtml = '<li class="item-content"><div class="item-inner"><div class="item-title">'+ r.strMeal +'</div></div></li>';
+            let liHtml = '<li class="item-content"><div class="item-inner"><div class="item-title"><a href="/recipe-details/'+ r.idMeal +'/">'+ r.strMeal +'</a></div></div></li>';
             ul.insertAdjacentHTML("afterBegin",liHtml);
         }
     });
 }
 
-function getTitle(title){
-    break
-}
 
